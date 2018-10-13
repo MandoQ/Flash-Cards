fedb07b7e611f1ec705a0399bf2b499f4b6a0ecb
Armando Q <qmando98@gmail.com>
2018-10-13 16:19:29 -0500

    Finished Lab1-FlashCards

---
+++ Flash Cards/SecondViewController.swift
@@ -0,0 +1,37 @@
+//
+//  SecondViewController.swift
+//  Flash Cards
+//
+//  Created by Mando Quintana on 10/13/18.
+//  Copyright © 2018 Armando Quintana. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet var welcome: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Flash Cards/ViewController.swift
+++ Flash Cards/ViewController.swift
@@ -8,11 +8,20 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    @IBOutlet var frontLabel: UILabel!
+    @IBOutlet var backLabel: UILabel!
+    @IBOutlet var choice1: UILabel!
+    @IBOutlet var choice2: UILabel!
+    @IBOutlet var choice3: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func didTapFlashCard(_ sender: Any) {
+        self.frontLabel.isHidden = true
+        self.backLabel.isHidden = false
+        print("pressed1")
+    }
+    
+    @IBAction func backToQuestion(_ sender: Any) {
+        self.frontLabel.isHidden = false
+        self.backLabel.isHidden = true
+    }
+    
+    @IBAction func didTapChoice1(_ sender: Any) {
+        choice1.backgroundColor = UIColor.green
+    }
+    
+    @IBAction func didTapChoice2(_ sender: Any) {
+        choice2.backgroundColor = UIColor.red
+    }
+    
+    @IBAction func didTapChoice3(_ sender: Any) {
+        choice3.backgroundColor = UIColor.red
+    }
+    
+    @IBAction func restTap(_ sender: Any) {
+        choice1.backgroundColor = UIColor.white
+        choice2.backgroundColor = UIColor.white
+        choice3.backgroundColor = UIColor.white
+        frontLabel.isHidden = false
+        backLabel.isHidden = true
+    }
 }
 
