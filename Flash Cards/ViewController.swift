fafc897ba2ca5363429b6e84c660870ba84a8e15
Armando Q <qmando98@gmail.com>
2018-10-13 01:06:45 -0500

    Initial Commit

---
+++ Flash Cards/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flash Cards
+//
+//  Created by Mando Quintana on 10/13/18.
+//  Copyright © 2018 Armando Quintana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
